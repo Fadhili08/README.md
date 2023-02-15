1ed1e2562ce3426c5b364287735d6b9a7a2f6a1e
Alvin <alvinsereno917@gmail.com>
2023-02-15 10:50:44 +0300

    for_loop.py

--- Week-2/for_loop.py
+++ Week-2/for_loop.py
@@ -1,2 +1,18 @@
 for i in range(0,10):
     print(i)
+for i in range (20,100):
+    print(i)
+    sum = 0
+    for i in range(0,10):
+        print(i)
+for y in range(0,100):
+    print(y)
+    sum = sum + i
+print(sum)
+product = 1 
+for x in range(1,10):
+    product = product * x 
+
+print(product)
+#factorial 
+# 6!
