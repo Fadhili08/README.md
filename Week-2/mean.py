352eed92d25786e1f0b2e81950ed567775beb0c4
Alvin <alvinsereno917@gmail.com>
2023-02-17 11:27:31 +0300

    strings in python

--- Week-2/mean.py
+++ Week-2/mean.py
@@ -1,11 +1,8 @@
-number - (1,2,3,4,5,6,7,8,9)
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,]
 
-sum_num =0
-for number in numbers:
+sum_num = 0
+for number in numbers :
     sum_num = sum_num + number 
 
-
-avg_num = sum_num /10 
-print("the average of the number :"), avg_num 
-
-
+avg_num = sum_num / 10
+print("the average of the number is : ",avg_num)
