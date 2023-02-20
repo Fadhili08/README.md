78cab323138f2309c283a720f368278ffc0ab51a
Alvin <alvinsereno917@gmail.com>
2023-02-20 09:48:32 +0300

    data_types

---
+++ Week-3/assignment.py
@@ -0,0 +1,6 @@
+favourite_musicians =[ ]
+#using a for loop add 7 new musicians 
+#copy the list to a new list clled musicians 
+#sort the list 
+#Pop out 2 celebrities from the list 
+#Count the remaining celebrities in the list 
---
+++ Week-3/lists_revisited.py
@@ -0,0 +1,7 @@
+friends = ["Henry , Mark , John , Phoebe , Tim"]
+print(friends)
+new_friends = friends.copy()
+print(new_friends)
+
+new_friends.sort()
+new_friends.pop()
