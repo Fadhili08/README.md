13c83f66d023301a8d418c0bd37dd20fe60c4639
Alvin <alvinsereno917@gmail.com>
2023-02-20 11:05:54 +0300

    strings in python

--- Week-2/bank.py
+++ Week-2/bank.py
@@ -6,4 +6,23 @@
 #5% if income is less 100k 
 
 # print gross income , net income 
-income = input(" enter your net income")
+
+gross_income = int(input("what is your gross incomee "))
+tax_group_A = (gross_income * 0.05)
+tax_group_B = (gross_income * 0.16)
+tax_group_C = (gross_income * 0.19)
+tax_group_D = (gross_income * 0.30)
+
+if gross_income < 100000:
+    print("gross income", gross_income) 
+    print("net income", gross_income - tax_group_A) 
+elif(gross_income > 100000) & (gross_income < 150000):
+  print("gross income:", gross_income)
+  print("net income:", gross_income - tax_group_B)
+
+elif(gross_income >= 150000) & (gross_income <= 300000):
+    print("gross income:", gross_income)
+    print("net income:", gross_income - tax_group_C)
+else: 
+     print("gross income:", gross_income)
+     print("net income:", gross_income - tax_group_D)
