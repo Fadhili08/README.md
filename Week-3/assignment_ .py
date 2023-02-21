aa899dfdf9077452e17b1c7d5c50a5925da074fa
Alvin <alvinsereno917@gmail.com>
2023-02-21 11:27:27 +0300

    data_types

---
+++ Week-3/assignment_ .py
@@ -0,0 +1,5 @@
+#write a program that solves a quadratic equation 
+#Using for loop 
+#Draw a diamond 
+#draw a triangle 
+# Pascals triangle 
---
+++ Week-3/loops.py
@@ -0,0 +1,8 @@
+
+if(age <= 25 & gender ="Male"):
+    if(weight =>75 | height < 125):
+       print("You are obese")
+    elif(weight =>55 | height <115)
+          print("you are healthy")
+elif(age <= 18 & gender = "Male"):
+    
