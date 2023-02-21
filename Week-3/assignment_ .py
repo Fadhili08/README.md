52505ae82465031d1820cb29d32949b13f5d0bd4
Alvin <alvinsereno917@gmail.com>
2023-02-21 23:55:58 +0300

    data_types

--- Week-3/assignment_ .py
+++ Week-3/assignment_ .py
@@ -16,32 +16,32 @@ ans2 = -abs(second_coefficient) + cmath.sqrt((second_coefficient**2)-(4*first_co
 print("The answers are "+str(ans1)+" and "+str(ans2))
 
 #using a for loop draw a diamond
-R = int(input("Enter the range: "))
-for diamond in range(R):
-    print("  " * (R - diamond), " *" * (2 * diamond +1))
-for diamond in range(R-2,-1,-1):
-    print("  " * (R - diamond), " *" * (2 * diamond +1))
+M = int(input("Enter the range: "))
+for diamond in range(M):
+    print("  " * (M - diamond), " *" * (2 * diamond +1))
+for diamond in range(M-2,-1,-1):
+    print("  " * (M - diamond), " *" * (2 * diamond +1))
 
 print("-----------------------------------------")
 
 #using a for loop draw a triangle
-for diamond in range(R):
-    print("  " * (R - diamond), " *" * (2 * diamond +1))
+for diamond in range(M):
+    print("  " * (M - diamond), " *" * (2 * diamond +1))
 
 print("------------------------------------------")
 
 #using a for loop draw pascals triangle
-for i in range(1, R+1):
-	for j in range(0, R-i+1):
+for l in range(1, M+1):
+	for p in range(0, M-l+1):
 		print(' ', end='')
 
 	# first element is always 1
 	C = 1
-	for j in range(1, i+1):
+	for p in range(1, l+1):
 
 		# first value in a line is always 1
 		print(' ', C, sep='', end='')
 
-		# using Binomial Coefficient
-		C = C * (i - j) // j
+		#  Binomial Coefficient
+		C = C * (l - p) // p
 	print()
--- Week-3/loops.py
+++ Week-3/loops.py
@@ -5,7 +5,7 @@ gender = "male"
 height = 140
 weight = 70
 
-if( age <= 18) & (gender = male) :
+if( age <= 18) & (gender  male) :
     if (weight > 65 | height < 175):
         print("You are obese")
     else:
