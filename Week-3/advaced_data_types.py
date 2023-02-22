df27953f8731a8873efebd62aafa462ee4325dc7
Alvin <alvinsereno917@gmail.com>
2023-02-22 10:43:39 +0300

    data_tyes

---
+++ Week-3/advaced_data_types.py
@@ -0,0 +1,52 @@
+#advanced data types
+
+#Mutable VSimmutable
+
+#Mutable are data types that can change during progrom life cycle 
+#add/ remove elements 
+#Immutable--> data types that can't be edited 
+
+#1 list (mutable)
+
+friends = ["Jeff","Norm" , "Anita" , "Jesse" , "Nicole"]
+#or friends 
+#add --> append(), extend()
+#remove--> pop()
+
+Students = ["Victor", "Dave", "Mike"]
+
+friends.extend(Students)
+friends.append("victor")
+friends.reverse()
+# 2) Tuples (immutable)
+#types of list that are immutable 
+
+stationaries = ("pens", "ink","sharpener",)
+
+#replace the whole tuple
+stationaries = ("ruler", "clip board", "eraser")
+
+
+for stationary in  stationaries:
+    print(stationary)
+
+numbers = (1,2,3,4,5,6,7,76,89,)
+
+#3) Dictionaries aka dict 
+
+#uses a key and a value pair to store data 
+
+Student = {"Name": "Alvin", "age":18 , "gender": "Male", "height" :"is_tall "}
+print(Student["Name"])
+print(Student["age"])
+print(Student["gender"])
+print(Student["height"])
+
+Friend = {"Fav_color": "Yellow", "hobbies": "dancing , singing", "course": "driving" , "weight":70 , "height":190}
+print(Friend["Fav_color"])
+print(Friend["hobbies"])
+print(Friend["course"])
+print(Friend["weight"])
+print(Friend["height"])
+print(Student.value())
+print (Student.keys())
---
+++ Week-3/maths_function.py
@@ -0,0 +1,20 @@
+import math
+
+#using the math library
+
+x = math.pow(2,4)
+print(x)
+#print("{} to power {} = {}", format(str(2)))
+
+#square root of 16 
+z = math.sqrt(16)
+
+#trigonometric values
+
+cos_x = math.cos(60)
+tan_x = math.cos(60)
+sin_x = math.cos(60)
+
+print(cos_x)
+print(tan_x)
+print(sin_x)
