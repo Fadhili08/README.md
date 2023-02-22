3c0b15e800a755fd21ef094c5400925a65c33cca
Alvin <alvinsereno917@gmail.com>
2023-02-22 11:00:24 +0300

    data_types

--- Week-3/continue.py
+++ Week-3/continue.py
@@ -1,8 +1,8 @@
 num = 0 
 while(num < 10 ):
-    num = num + 1
+    num = num + 0.5
     print(num)
-    if(num == 5):  
-      print("count has reached 5 ")
+    if(num == 5.0):  
+      print("count has reached 5.0 ")
       continue
 
